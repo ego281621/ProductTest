432b632f5219456f12d12bd0eb93842c94153c66
Eric Go <Eric@qualiticks.com>
2023-08-15 21:11:53 +0800

    added comments

--- Server/Repositories/Product/IProductRepository.cs
+++ Server/Repositories/Product/IProductRepository.cs
@@ -1,16 +1,44 @@
-
 using System.ComponentModel.DataAnnotations;
 using ProductTest.Shared.Models;
 
 namespace ProductTest.Server.Repositories.Product
 {
+    /// <summary>
+    /// Represents a repository interface for managing product data.
+    /// </summary>
     public interface IProductRepository
     {
+        /// <summary>
+        /// Retrieve all products.
+        /// </summary>
+        /// <returns>An enumerable collection of product models.</returns>
         IEnumerable<ProductModel> GetProducts();
-        ProductModel GetProduct(string id);
-        ProductModel AddProduct(ProductModel product);
-        ProductModel UpdateProduct(ProductModel product);
-        void DeleteProduct(string id);
 
+        /// <summary>
+        /// Retrieve a specific product by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the product to retrieve.</param>
+        /// <returns>A product model if found, otherwise null.</returns>
+        ProductModel GetProduct(string id);
+
+        /// <summary>
+        /// Add a new product.
+        /// </summary>
+        /// <param name="product">The product model to be added.</param>
+        /// <returns>The added product model.</returns>
+        ProductModel AddProduct(ProductModel product);
+
+        /// <summary>
+        /// Update an existing product.
+        /// </summary>
+        /// <param name="product">The updated product model.</param>
+        /// <returns>The updated product model.</returns>
+        ProductModel UpdateProduct(ProductModel product);
+
+        /// <summary>
+        /// Delete a product by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the product to be deleted.</param>
+        void DeleteProduct(string id);
     }
 }
