f461eba51eca6701ba4d7f08349df7f78a7e4d66
Eric Go <Eric@qualiticks.com>
2023-08-15 21:15:58 +0800

    update comments

--- Client/ApiClient/ProductsApiClient.cs
+++ Client/ApiClient/ProductsApiClient.cs
@@ -6,41 +6,68 @@ using static System.Net.WebRequestMethods;
 
 namespace ProductTest.Client
 {
+    /// <summary>
+    /// Represents a client for making API requests related to products.
+    /// </summary>
     public class ProductsApiClient
     {
         private readonly HttpClient http;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductsApiClient"/> class.
+        /// </summary>
+        /// <param name="http">The HTTP client for making requests.</param>
         public ProductsApiClient(HttpClient http)
         {
             this.http = http;
         }
 
+        /// <summary>
+        /// Retrieves a list of products asynchronously.
+        /// </summary>
+        /// <returns>A list of product models.</returns>
         public virtual async Task<List<ProductModel>> GetProductsAsync()
         {
             return await http.GetFromJsonAsync<List<ProductModel>>("/api/products");
         }
 
+        /// <summary>
+        /// Retrieves a product by its ID asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the product to retrieve.</param>
+        /// <returns>The retrieved product model.</returns>
         public virtual async Task<ProductModel> GetProductAsync(string id)
         {
             return await http.GetFromJsonAsync<ProductModel>($"/api/products/{id}");
         }
 
+        /// <summary>
+        /// Adds a new product asynchronously.
+        /// </summary>
+        /// <param name="product">The product model to add.</param>
         public virtual async Task AddProductAsync(ProductModel product)
         {
             await http.PostAsJsonAsync("/api/products", product);
         }
 
+        /// <summary>
+        /// Updates an existing product asynchronously.
+        /// </summary>
+        /// <param name="product">The updated product model.</param>
         public virtual async Task UpdateProductAsync(ProductModel product)
         {
             await http.PutAsJsonAsync("/api/products", product);
         }
 
+        /// <summary>
+        /// Deletes a product by its ID asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the product to delete.</param>
         public virtual async Task DeleteProductAsync(string id)
         {
             await http.DeleteAsync($"/api/products/{id}");
         }
 
-
-        // other CRUD methods
+        // Other CRUD methods can be added here
     }
 }
--- Server/Models/ProductsDbContext.cs
+++ Server/Models/ProductsDbContext.cs
@@ -2,39 +2,52 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace ProductTest.Server.Models;
-
-public partial class ProductsDbContext : DbContext
+namespace ProductTest.Server.Models
 {
-    public ProductsDbContext()
+    /// <summary>
+    /// Represents the database context for managing products.
+    /// </summary>
+    public partial class ProductsDbContext : DbContext
     {
-    }
-
-    public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Product> Products { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Product>(entity =>
+        public ProductsDbContext()
         {
-            entity.ToTable("Product");
+        }
 
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.Description)
-                .HasMaxLength(500)
-                .IsUnicode(false);
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .IsUnicode(false);
-            entity.Property(e => e.Price).HasColumnType("decimal(18, 9)");
-        });
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+            : base(options)
+        {
+        }
 
-        OnModelCreatingPartial(modelBuilder);
+        /// <summary>
+        /// Gets or sets the DbSet of products in the database.
+        /// </summary>
+        public virtual DbSet<Product> Products { get; set; }
+
+        /// <summary>
+        /// Configures the entity models and relationships for the database context.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                // Configure properties of the "Product" entity
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 9)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        // Partial method that can be implemented in a separate file
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
